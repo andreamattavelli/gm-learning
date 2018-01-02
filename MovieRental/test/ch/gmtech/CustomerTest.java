78f82e30d0a11e51ea319e866f19c2b1be77201c
Andrea Mattavelli <andreamattavelli@gmail.com>
2018-01-02 17:07:49 +0100

    Refactoring up to replacing conditional with polymorphism

--- MovieRental/test/ch/gmtech/CustomerTest.java
+++ MovieRental/test/ch/gmtech/CustomerTest.java
@@ -13,6 +13,7 @@ public class CustomerTest {
 		Rental rental0 = new Rental(movie0, 2);
 		customer0.addRental(rental0);
 		assertEquals("Rental Record for Name Surname\n\tMega\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points", customer0.statement());
+		assertEquals("<H1>Rentals for <EM>Name Surname</EM></H1><P>\nMega: 1.5<BR>\n<P>You owe <EM>1.5</EM><P>\nOn this rental you earned <EM>1</EM> frequent renter points<P>", customer0.htmlStatement());
 	}
 
 	@Test
@@ -22,6 +23,7 @@ public class CustomerTest {
 		Rental rental0 = new Rental(movie0, 2);
 		customer0.addRental(rental0);
 		assertEquals("Rental Record for Name Surname\n\tMega\t2.0\nAmount owed is 2.0\nYou earned 1 frequent renter points", customer0.statement());
+		assertEquals("<H1>Rentals for <EM>Name Surname</EM></H1><P>\nMega: 2.0<BR>\n<P>You owe <EM>2.0</EM><P>\nOn this rental you earned <EM>1</EM> frequent renter points<P>", customer0.htmlStatement());
 	}
 
 	@Test
@@ -31,6 +33,7 @@ public class CustomerTest {
 		Rental rental0 = new Rental(movie0, 2);
 		customer0.addRental(rental0);
 		assertEquals("Rental Record for Name Surname\n\tMega\t6.0\nAmount owed is 6.0\nYou earned 2 frequent renter points", customer0.statement());
+		assertEquals("<H1>Rentals for <EM>Name Surname</EM></H1><P>\nMega: 6.0<BR>\n<P>You owe <EM>6.0</EM><P>\nOn this rental you earned <EM>2</EM> frequent renter points<P>", customer0.htmlStatement());
 	}
 
 	@Test
@@ -40,6 +43,7 @@ public class CustomerTest {
 		Rental rental0 = new Rental(movie0, 557);
 		customer0.addRental(rental0);
 		assertEquals("Rental Record for Name Surname\n\tMega\t832.5\nAmount owed is 832.5\nYou earned 1 frequent renter points", customer0.statement());
+		assertEquals("<H1>Rentals for <EM>Name Surname</EM></H1><P>\nMega: 832.5<BR>\n<P>You owe <EM>832.5</EM><P>\nOn this rental you earned <EM>1</EM> frequent renter points<P>", customer0.htmlStatement());
 	}
 
 }
