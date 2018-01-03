289d2fb69fb2f56b13fb62d48788cb1b12bf71f7
Andrea Mattavelli <andreamattavelli@gmail.com>
2018-01-03 10:29:13 +0100

    Refactored code up to end of chapter 1

---
+++ MovieRental/src/ch/gmtech/NewReleasePrice.java
@@ -0,0 +1,18 @@
+package ch.gmtech;
+
+public class NewReleasePrice extends Price {
+
+	@Override
+	public int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+	
+	public double getCharge(int daysRented) {
+		return (double) (daysRented * 3);
+	}
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
+	}
+
+}
--- MovieRental/test/ch/gmtech/MovieTest.java
+++ MovieRental/test/ch/gmtech/MovieTest.java
@@ -8,9 +8,9 @@ class MovieTest {
 
 	  @Test
 	  public void test0()  throws Throwable  {
-	      Movie movie0 = new Movie("Mega", 2013);
+	      Movie movie0 = new Movie("Mega", 1);
 	      assertEquals("Mega", movie0.getTitle());
-	      assertEquals(2013, movie0.getPriceCode());
+	      assertEquals(1, movie0.getPriceCode());
 	  }
 
 }
