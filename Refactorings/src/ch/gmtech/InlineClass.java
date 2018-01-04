ed037704544332ab380324bb1d7a8d3d814c3800
Andrea Mattavelli <andreamattavelli@gmail.com>
2018-01-04 14:58:24 +0100

    Added Inline Class

---
+++ Refactorings/src/ch/gmtech/InlineClass.java
@@ -0,0 +1,76 @@
+package ch.gmtech;
+
+public class InlineClass {
+
+//	class Person {
+//		public String getName() {
+//			return _name;
+//		}
+//
+//		public String getTelephoneNumber() {
+//			return _officeTelephone.getTelephoneNumber();
+//		}
+//
+//		TelephoneNumber getOfficeTelephone() {
+//			return _officeTelephone;
+//		}
+//
+//		private String _name;
+//		private TelephoneNumber _officeTelephone = new TelephoneNumber();
+//	}
+//
+//	class TelephoneNumber {
+//		public String getTelephoneNumber() {
+//			return ("(" + _areaCode + ") " + _number);
+//		}
+//
+//		String getAreaCode() {
+//			return _areaCode;
+//		}
+//
+//		void setAreaCode(String arg) {
+//			_areaCode = arg;
+//		}
+//
+//		String getNumber() {
+//			return _number;
+//		}
+//
+//		void setNumber(String arg) {
+//			_number = arg;
+//		}
+//
+//		private String _number;
+//		private String _areaCode;
+//	}
+	
+	class Person {
+		public String getName() {
+			return _name;
+		}
+
+		public String getTelephoneNumber() {
+			return ("(" + _areaCode + ") " + _number);
+		}
+
+		String getAreaCode() {
+			return _areaCode;
+		}
+
+		void setAreaCode(String arg) {
+			_areaCode = arg;
+		}
+
+		String getNumber() {
+			return _number;
+		}
+
+		void setNumber(String arg) {
+			_number = arg;
+		}
+
+		private String _number;
+		private String _areaCode;
+		private String _name;
+	}
+}
